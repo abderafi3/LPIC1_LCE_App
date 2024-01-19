24bedd6c15f828f222e727ceb2f7cbad0ede4076
abderafi3 <abderafi3@gmail.com>
2024-01-19 11:51:12 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'LPIC-1_LCE_App';
+}
